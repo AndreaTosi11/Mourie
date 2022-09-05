42cfe339184b7ab6834ab1fd9232b6577038f7ea
AndreTo11 <andrea.tosi11@yahoo.com>
2022-09-05 19:32:20 +0200

    seconda prova, migliore, correggere index witdth not 100%

---
+++ Mourie Prova 2/Controllers/OtherPages.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mourie_Prova_2.Models;
+using System.Diagnostics;
+
+namespace Mourie_Prova_2.Controllers
+{
+	public class OtherPages : Controller
+	{
+        private readonly ILogger<OtherPages> _logger;
+
+        public OtherPages(ILogger<OtherPages> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Collabora()
+        {
+            return View();
+        }
+        public IActionResult RltS()
+        {
+            return View();
+        }
+    }
+}
